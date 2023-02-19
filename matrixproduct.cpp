068725b3dab4f111f9f4dda22558eba44ce6b608
Miguel Lopes <lopesazevedom@gmail.com>
2023-02-19 17:05:05 +0000

    some restructuring

--- matrixproduct.cpp
+++ matrixproduct.cpp
@@ -11,9 +11,12 @@ void OnMult(int m_ar, int m_br, ofstream &out)
 
     SYSTEMTIME Time1, Time2;
 
-    char st[100];
+
+    char st[200];
     double temp;
     int i, j, k;
+
+    out << m_ar<<"x"<<m_br<<"Simple:"<< endl;
 
     double *pha, *phb, *phc;
 
@@ -29,8 +32,12 @@ void OnMult(int m_ar, int m_br, ofstream &out)
         for (j = 0; j < m_br; j++)
             phb[i * m_br + j] = (double)(i + 1);
 
-    Time1 = clock();
+    size_t count;
+    emlDeviceGetCount(&count);
+    emlData_t* data[count];
 
+    Time1 = clock();
+    emlStart();
     for (i = 0; i < m_ar; i++)
     {
         for (j = 0; j < m_br; j++)
@@ -43,8 +50,17 @@ void OnMult(int m_ar, int m_br, ofstream &out)
             phc[i * m_ar + j] = temp;
         }
     }
-
+    emlStop(data);
     Time2 = clock();
+
+    double consumed, elapsed;
+    emlDataGetConsumed(data[0], &consumed);
+    emlDataGetElapsed(data[0], &elapsed);
+    emlDataFree(data[0]);
+
+    sprintf(st,"This device consumed %g J in %g s\n", consumed, elapsed);
+    out << st;
+
     sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
     out << st;
 
@@ -55,7 +71,7 @@ void OnMult(int m_ar, int m_br, ofstream &out)
         for (j = 0; j < min(10, m_br); j++)
             out << phc[j] << " ";
     }
-    out << endl;
+    out << endl<<endl;
     cout<<"End mult matrix"<<endl;
 
     free(pha);
@@ -68,7 +84,7 @@ void OnMultLine(int m_ar, int m_br, ofstream &out)
 {
     SYSTEMTIME Time1, Time2;
 
-    char st[100];
+    char st[200];
     double temp;
     int i, j, k;
 
@@ -77,6 +93,9 @@ void OnMultLine(int m_ar, int m_br, ofstream &out)
     pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
     phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
     phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+    out << m_ar<<"x"<<m_br<<"MultLine:"<< endl;
+
 
     for (i = 0; i < m_ar; i++){
         for (j = 0; j < m_ar; j++){
@@ -91,7 +110,12 @@ void OnMultLine(int m_ar, int m_br, ofstream &out)
         }
     }
 
+    size_t count;
+    emlDeviceGetCount(&count);
+    emlData_t* data[count];
+
     Time1 = clock();
+    emlStart();
 
     for (i = 0; i < m_ar; i++) {
         for (k = 0; k < m_br; k++) {
@@ -101,7 +125,17 @@ void OnMultLine(int m_ar, int m_br, ofstream &out)
         }
     }
 
+    emlStop(data);
     Time2 = clock();
+
+    double consumed, elapsed;
+    emlDataGetConsumed(data[0], &consumed);
+    emlDataGetElapsed(data[0], &elapsed);
+    emlDataFree(data[0]);
+
+    sprintf(st,"This device consumed %g J in %g s\n", consumed, elapsed);
+    out << st;
+
     sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
     out << st;
 
@@ -112,7 +146,7 @@ void OnMultLine(int m_ar, int m_br, ofstream &out)
         for (j = 0; j < min(10, m_br); j++)
             out << phc[j] << " ";
     }
-    out << endl;
+    out << endl<<endl;
     cout<<"End multline matrix"<<endl;
 
     free(pha);
@@ -127,78 +161,18 @@ int main(int argc, char *argv[])
     ofstream outputFile;
     outputFile.open("output1.txt");
 
-    size_t count;
-    emlDeviceGetCount(&count);
-    emlData_t* data[count];
-    
-    printf("1000x1000 Simple:\n");
-    emlStart();
 
     OnMult(1000, 1000, outputFile);
 
-    emlStop(data);
-    double consumed, elapsed;
-    emlDataGetConsumed(data[0], &consumed);
-    emlDataGetElapsed(data[0], &elapsed);
-    emlDataFree(data[0]);
-    printf("This device consumed %g J in %g s\n\n", consumed, elapsed);
-
-    printf("1400x1400 Simple:\n");
-    emlStart();
-
     OnMult(1400, 1400, outputFile);
-
-    emlStop(data);
-    emlDataGetConsumed(data[0], &consumed);
-    emlDataGetElapsed(data[0], &elapsed);
-    emlDataFree(data[0]);
-    printf("This device consumed %g J in %g s\n\n", consumed, elapsed);
-
-    printf("2000x2000 Simple:\n");
-    emlStart();
 
     OnMult(2000, 2000, outputFile);
 
-    emlStop(data);
-    emlDataGetConsumed(data[0], &consumed);
-    emlDataGetElapsed(data[0], &elapsed);
-    emlDataFree(data[0]);
-    printf("This device consumed %g J in %g s\n\n", consumed, elapsed);
-
-
-    printf("1000x1000 MultLine:\n");
-    emlStart();
-
     OnMultLine(1000, 1000, outputFile);
-
-    emlStop(data);
-    emlDataGetConsumed(data[0], &consumed);
-    emlDataGetElapsed(data[0], &elapsed);
-    emlDataFree(data[0]);
-    printf("This device consumed %g J in %g s\n\n", consumed, elapsed);
-
-    printf("1400x1400 MultLine:\n");
-    emlStart();
 
     OnMultLine(1400, 1400, outputFile);
 
-    emlStop(data);
-    emlDataGetConsumed(data[0], &consumed);
-    emlDataGetElapsed(data[0], &elapsed);
-    emlDataFree(data[0]);
-    printf("This device consumed %g J in %g s\n\n", consumed, elapsed);
-
-    printf("2000x2000 MultLine:\n");
-    emlStart();
-
     OnMultLine(2000, 2000, outputFile);
-
-    emlStop(data);
-    emlDataGetConsumed(data[0], &consumed);
-    emlDataGetElapsed(data[0], &elapsed);
-    emlDataFree(data[0]);
-    printf("This device consumed %g J in %g s\n\n", consumed, elapsed);
-
 
     outputFile.close();
     emlShutdown();
