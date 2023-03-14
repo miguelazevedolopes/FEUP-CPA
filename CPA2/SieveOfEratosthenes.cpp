813ae2518b1ed4ddc6e2a6e65ef6f3de2f89395e
Joel Fernandes <up201904977@fe.up.pt>
2023-03-14 16:03:09 +0000

    chore: add other function templates

--- CPA2/SieveOfEratosthenes.cpp
+++ CPA2/SieveOfEratosthenes.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <cmath>
 
 void SieveOfEratosthenes(long long n, std::ofstream &outputFile)
 {
@@ -50,10 +51,117 @@ void SieveOfEratosthenes(long long n, std::ofstream &outputFile)
     delete marks;
 }
 
+// TODO
+void SieveOfEratosthenesFastMarking(long long n, std::ofstream &outputFile)
+{
+    // Array in style -> [2, 3, 4, 5, 6, 7, 8, ..., n]
+    // Size is n - 1
+    long long size = n - 1;
+    bool *marks = new bool[size];
+
+    // Start time
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+    // Calculate primes
+    for (long long k = 0; k < size; k++)
+    {
+        // Number is always 2 + index of array
+        long long num = k + 2;
+
+        // Go from num * num until n to check for multiples
+        for (long long j = num * num - 2; j < size; j++)
+        {
+            // Number is always 2 + index of array
+            long long multNum = j + 2;
+
+            // Mark number if divisible by study number
+            marks[j] |= (multNum % num == 0);
+        }
+    }
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+    // Output Execution time
+    outputFile << "Elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() / 1000000.0 << "s" << std::endl
+               << std::endl;
+
+    // Output Primes
+    for (long long k = 0; k < size; k++)
+    {
+        // Unmarked are prime
+        if (!marks[k])
+        {
+            // Number is always 2 + index of array
+            outputFile << k + 2 << std::endl;
+        }
+    }
+
+    // Clear memory :D
+    delete marks;
+}
+
+// TODO
+void SieveOfEratosthenesFastMarkingReorganized(long long n, std::ofstream &outputFile)
+{
+    // Array in style -> [2, 3, 4, 5, 6, 7, 8, ..., n]
+    // Size is n - 1
+    long long size = n - 1;
+    bool *marks = new bool[size];
+
+    // Start time
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+    // Calculate primes
+    for (long long k = 0; k < size; k++)
+    {
+        // Number is always 2 + index of array
+        long long num = k + 2;
+
+        // Go from num * num until n to check for multiples
+        for (long long j = num * num - 2; j < size; j++)
+        {
+            // Number is always 2 + index of array
+            long long multNum = j + 2;
+
+            // Mark number if divisible by study number
+            marks[j] |= (multNum % num == 0);
+        }
+    }
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+    // Output Execution time
+    outputFile << "Elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() / 1000000.0 << "s" << std::endl
+               << std::endl;
+
+    // Output Primes
+    for (long long k = 0; k < size; k++)
+    {
+        // Unmarked are prime
+        if (!marks[k])
+        {
+            // Number is always 2 + index of array
+            outputFile << k + 2 << std::endl;
+        }
+    }
+
+    // Clear memory :D
+    delete marks;
+}
+
 int main(int argc, char *argv[])
 {
-    std::ofstream outputFile;
-    outputFile.open("sieveoferatosthenes.txt");
+    long long test = 100;
 
-    SieveOfEratosthenes(1000000, outputFile);
+    std::ofstream outputFile1;
+    outputFile1.open("sieveoferatosthenes.txt");
+    SieveOfEratosthenes(test, outputFile1);
+
+    std::ofstream outputFile2;
+    outputFile2.open("sieveoferatosthenesfastmarking.txt");
+    SieveOfEratosthenesFastMarking(test, outputFile2);
+
+    std::ofstream outputFile3;
+    outputFile3.open("sieveoferatosthenesfastmarkingreorganized.txt");
+    SieveOfEratosthenesFastMarkingReorganized(test, outputFile3);
 }
