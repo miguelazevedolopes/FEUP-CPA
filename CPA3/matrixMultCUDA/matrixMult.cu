8287c96617684a9f8f8957dc3d6e9ddc5b290c94
Joel Fernandes <up201904977@fe.up.pt>
2023-04-23 14:36:22 +0100

    feat: add matrixMult CUDA example

---
+++ CPA3/matrixMultCUDA/matrixMult.cu
@@ -0,0 +1,242 @@
+// System includes
+#include <stdio.h>
+#include <assert.h>
+
+// CUDA runtime
+#include <cuda_runtime.h>
+#include <cuda_profiler_api.h>
+
+// CUDA Kernel
+template <int BLOCK_SIZE>
+__global__ void MatrixMulCUDA(float *C, float *A,
+                              float *B, int wA,
+                              int wB)
+{
+  // Block index
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+
+  // Thread index
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+
+  // Index of the first sub-matrix of A processed by the block
+  int aBegin = wA * BLOCK_SIZE * by;
+
+  // Index of the last sub-matrix of A processed by the block
+  int aEnd = aBegin + wA - 1;
+
+  // Step size used to iterate through the sub-matrices of A
+  int aStep = BLOCK_SIZE;
+
+  // Index of the first sub-matrix of B processed by the block
+  int bBegin = BLOCK_SIZE * bx;
+
+  // Step size used to iterate through the sub-matrices of B
+  int bStep = BLOCK_SIZE * wB;
+
+  // Csub is used to store the element of the block sub-matrix
+  // that is computed by the thread
+  float Csub = 0;
+
+  // Loop over all the sub-matrices of A and B
+  // required to compute the block sub-matrix
+  for (int a = aBegin, b = bBegin;
+       a <= aEnd;
+       a += aStep, b += bStep)
+  {
+    // Declaration of the shared memory array As used to
+    // store the sub-matrix of A
+    __shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+
+    // Declaration of the shared memory array Bs used to
+    // store the sub-matrix of B
+    __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
+
+    // Load the matrices from device memory
+    // to shared memory; each thread loads
+    // one element of each matrix
+    As[ty][tx] = A[a + wA * ty + tx];
+    Bs[ty][tx] = B[b + wB * ty + tx];
+
+    // Synchronize to make sure the matrices are loaded
+    __syncthreads();
+
+    // Multiply the two matrices together;
+    // each thread computes one element
+    // of the block sub-matrix
+#pragma unroll
+
+    for (int k = 0; k < BLOCK_SIZE; ++k)
+    {
+      Csub += As[ty][k] * Bs[k][tx];
+    }
+
+    // Synchronize to make sure that the preceding
+    // computation is done before loading two new
+    // sub-matrices of A and B in the next iteration
+    __syncthreads();
+  }
+
+  // Write the block sub-matrix to device memory;
+  // each thread writes one element
+  int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+  C[c + wB * ty + tx] = Csub;
+}
+
+// Aux functions
+void checkCudaErrors(cudaError_t err)
+{
+  if (err != cudaSuccess)
+  {
+    fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(err));
+    exit(-1);
+  }
+}
+
+void ConstantInit(float *data, int size, float val)
+{
+  for (int i = 0; i < size; ++i)
+  {
+    data[i] = val;
+  }
+}
+
+// Main multiply function
+int MatrixMultiply(int block_size, const dim3 &dimsA,
+                   const dim3 &dimsB)
+{
+  // Allocate host memory for matrices A and B
+  unsigned int size_A = dimsA.x * dimsA.y;
+  unsigned int mem_size_A = sizeof(float) * size_A;
+  float *h_A;
+  checkCudaErrors(cudaMallocHost(&h_A, mem_size_A));
+  unsigned int size_B = dimsB.x * dimsB.y;
+  unsigned int mem_size_B = sizeof(float) * size_B;
+  float *h_B;
+  checkCudaErrors(cudaMallocHost(&h_B, mem_size_B));
+  cudaStream_t stream;
+
+  // Initialize host memory
+  const float valB = 0.01f;
+  ConstantInit(h_A, size_A, 1.0f);
+  ConstantInit(h_B, size_B, valB);
+
+  // Allocate device memory
+  float *d_A, *d_B, *d_C;
+
+  // Allocate host matrix C
+  dim3 dimsC(dimsB.x, dimsA.y, 1);
+  unsigned int mem_size_C = dimsC.x * dimsC.y * sizeof(float);
+  float *h_C;
+  checkCudaErrors(cudaMallocHost(&h_C, mem_size_C));
+
+  if (h_C == NULL)
+  {
+    fprintf(stderr, "Failed to allocate host matrix C!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&d_A), mem_size_A));
+  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&d_B), mem_size_B));
+  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&d_C), mem_size_C));
+
+  // Allocate CUDA events that we'll use for timing
+  cudaEvent_t start, stop;
+  checkCudaErrors(cudaEventCreate(&start));
+  checkCudaErrors(cudaEventCreate(&stop));
+
+  checkCudaErrors(cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking));
+
+  // copy host memory to device
+  checkCudaErrors(
+      cudaMemcpyAsync(d_A, h_A, mem_size_A, cudaMemcpyHostToDevice, stream));
+  checkCudaErrors(
+      cudaMemcpyAsync(d_B, h_B, mem_size_B, cudaMemcpyHostToDevice, stream));
+
+  // Setup execution parameters
+  dim3 threads(block_size, block_size);
+  dim3 grid(dimsB.x / threads.x, dimsA.y / threads.y);
+
+  // Create and start timer
+  printf("Computing result using CUDA Kernel...\n");
+
+  checkCudaErrors(cudaStreamSynchronize(stream));
+
+  // Record the start event
+  checkCudaErrors(cudaEventRecord(start, stream));
+
+  // Performs warmup operation using matrixMul CUDA kernel
+  if (block_size == 16)
+  {
+    MatrixMulCUDA<16>
+        <<<grid, threads, 0, stream>>>(d_C, d_A, d_B, dimsA.x, dimsB.x);
+  }
+  else
+  {
+    MatrixMulCUDA<32>
+        <<<grid, threads, 0, stream>>>(d_C, d_A, d_B, dimsA.x, dimsB.x);
+  }
+
+  printf("Done\n");
+
+  // Record the stop event
+  checkCudaErrors(cudaEventRecord(stop, stream));
+
+  // Wait for the stop event to complete
+  checkCudaErrors(cudaEventSynchronize(stop));
+
+  float msecTotal = 0.0f;
+  checkCudaErrors(cudaEventElapsedTime(&msecTotal, start, stop));
+
+  printf("Time = %.3f msec", msecTotal);
+
+  // Copy result from device to host
+  checkCudaErrors(
+      cudaMemcpyAsync(h_C, d_C, mem_size_C, cudaMemcpyDeviceToHost, stream));
+  checkCudaErrors(cudaStreamSynchronize(stream));
+
+  // Clean up memory
+  checkCudaErrors(cudaFreeHost(h_A));
+  checkCudaErrors(cudaFreeHost(h_B));
+  checkCudaErrors(cudaFreeHost(h_C));
+  checkCudaErrors(cudaFree(d_A));
+  checkCudaErrors(cudaFree(d_B));
+  checkCudaErrors(cudaFree(d_C));
+  checkCudaErrors(cudaEventDestroy(start));
+  checkCudaErrors(cudaEventDestroy(stop));
+
+  return 0;
+}
+
+/**
+ * Program main
+ */
+int main(int argc, char **argv)
+{
+  int block_size = 32;
+
+  dim3 dimsA(0, 0, 1);
+  dim3 dimsB(0, 0, 1);
+
+  // width of Matrix A
+  dimsA.x = 1024*8;
+  dimsA.y = 1024*8;
+
+  dimsB.x = 1024*8;
+  dimsB.y = 1024*8;
+
+  if (dimsA.x != dimsB.y)
+  {
+    printf("Error: outer matrix dimensions must be equal. (%d != %d)\n",
+           dimsA.x, dimsB.y);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("MatrixA(%d,%d), MatrixB(%d,%d)\n", dimsA.x, dimsA.y,
+         dimsB.x, dimsB.y);
+
+  int matrix_result = MatrixMultiply(block_size, dimsA, dimsB);
+
+  return matrix_result;
+}
