d9617589a16f9f2732019a6703dcc421bf49cbed
Joel Fernandes <up201904977@fe.up.pt>
2023-05-08 21:27:44 +0100

    fixes

--- CPA3/matrixMultSYCLOPENCL/main.cpp
+++ CPA3/matrixMultSYCLOPENCL/main.cpp
@@ -5,8 +5,10 @@
 #include <ctime>
 #include <iostream>
 #include <omp.h>
+#include <iomanip>
 
 using namespace cl::sycl;
+#define NSEC_IN_MSEC 1000000.0
 
 class mxm_kernel;
 
@@ -40,75 +42,79 @@ void block_host(float *MA, float *MB, float *MC, int matSize, int block_size)
 template <typename T>
 bool local_mxm(cl::sycl::queue &q, T *MA, T *MB, T *MC, int matSize, int blockSize)
 {
-        range<1> dimensions(matSize * matSize);
-        const property_list props = {property::buffer::use_host_ptr()};
-        buffer<T> bA(MA, dimensions, props);
-        buffer<T> bB(MB, dimensions, props);
-        buffer<T> bC(MC, dimensions, props);
+    range<1> dimensions(matSize * matSize);
+    const property_list props = {property::buffer::use_host_ptr()};
+    buffer<T> bA(MA, dimensions, props);
+    buffer<T> bB(MB, dimensions, props);
+    buffer<T> bC(MC, dimensions, props);
 
-        auto start = std::chrono::high_resolution_clock::now();
+    sycl::event event = q.submit([&](handler& cgh)
+        {
+            auto pA = bA.template get_access<access::mode::read>(cgh);
+            auto pB = bB.template get_access<access::mode::read>(cgh);
+            auto pC = bC.template get_access<access::mode::write>(cgh);
+            auto localRange = range<1>(blockSize * blockSize);
 
-        q.submit([&](handler& cgh)
-            {
-                auto pA = bA.template get_access<access::mode::read>(cgh);
-                auto pB = bB.template get_access<access::mode::read>(cgh);
-                auto pC = bC.template get_access<access::mode::write>(cgh);
-                auto localRange = range<1>(blockSize * blockSize);
+            accessor<T, 1, access::mode::read_write, access::target::local> pBA(
+                localRange, cgh);
+            accessor<T, 1, access::mode::read_write, access::target::local> pBB(
+                localRange, cgh);
 
-                accessor<T, 1, access::mode::read_write, access::target::local> pBA(
-                    localRange, cgh);
-                accessor<T, 1, access::mode::read_write, access::target::local> pBB(
-                    localRange, cgh);
+            cgh.parallel_for<mxm_kernel>(
+                nd_range<2>{range<2>(matSize, matSize),
+                range<2>(blockSize, blockSize)},
+                [=](nd_item<2> it) {
+                    // Current block
+                    int blockX = it.get_group(1);
+                    int blockY = it.get_group(0);
 
-                cgh.parallel_for<mxm_kernel>(
-                    nd_range<2>{range<2>(matSize, matSize),
-                    range<2>(blockSize, blockSize)},
-                    [=](nd_item<2> it) {
-                        // Current block
-                        int blockX = it.get_group(1);
-                        int blockY = it.get_group(0);
+                    // Current local item
+                    int localX = it.get_local_id(1);
+                    int localY = it.get_local_id(0);
 
-                        // Current local item
-                        int localX = it.get_local_id(1);
-                        int localY = it.get_local_id(0);
+                    // Start in the A matrix
+                    int a_start = matSize * blockSize * blockY;
+                    // End in the b matrix
+                    int a_end = a_start + matSize - 1;
+                    // Start in the b matrix
+                    int b_start = blockSize * blockX;
 
-                        // Start in the A matrix
-                        int a_start = matSize * blockSize * blockY;
-                        // End in the b matrix
-                        int a_end = a_start + matSize - 1;
-                        // Start in the b matrix
-                        int b_start = blockSize * blockX;
-
-                        // Result for the current C(i,j) element
-                        T tmp = 0.0f;
-                        // We go through all a, b blocks
-                        for (int a = a_start, b = b_start; a <= a_end;
-                            a += blockSize, b += (blockSize * matSize)) {
-                            // Copy the values in shared memory collectively
-                            pBA[localY * blockSize + localX] =
-                                pA[a + matSize * localY + localX];
-                            // Note the swap of X/Y to maintain contiguous access
-                            pBB[localX * blockSize + localY] =
-                                pB[b + matSize * localY + localX];
-                            it.barrier(access::fence_space::local_space);
-                            // Now each thread adds the value of its sum
-                            for (int k = 0; k < blockSize; k++) {
-                                tmp +=
-                                    pBA[localY * blockSize + k] * pBB[localX * blockSize + k];
-                            }
-                            // The barrier ensures that all threads have written to local
-                            // memory before continuing
-                            it.barrier(access::fence_space::local_space);
+                    // Result for the current C(i,j) element
+                    T tmp = 0.0f;
+                    // We go through all a, b blocks
+                    for (int a = a_start, b = b_start; a <= a_end;
+                        a += blockSize, b += (blockSize * matSize)) {
+                        // Copy the values in shared memory collectively
+                        pBA[localY * blockSize + localX] =
+                            pA[a + matSize * localY + localX];
+                        // Note the swap of X/Y to maintain contiguous access
+                        pBB[localX * blockSize + localY] =
+                            pB[b + matSize * localY + localX];
+                        it.barrier(access::fence_space::local_space);
+                        // Now each thread adds the value of its sum
+                        for (int k = 0; k < blockSize; k++) {
+                            tmp +=
+                                pBA[localY * blockSize + k] * pBB[localX * blockSize + k];
                         }
-                        auto elemIndex = it.get_global_id(0) * it.get_global_range()[1] +
-                            it.get_global_id(1);
-                        // Each thread updates its position
-                        pC[elemIndex] = tmp;
-                    });
+                        // The barrier ensures that all threads have written to local
+                        // memory before continuing
+                        it.barrier(access::fence_space::local_space);
+                    }
+                    auto elemIndex = it.get_global_id(0) * it.get_global_range()[1] +
+                        it.get_global_id(1);
+                    // Each thread updates its position
+                    pC[elemIndex] = tmp;
                 });
-    auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "Time = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl
-        << std::endl;
+            });
+
+    event.wait();
+    auto start =
+        event.get_profiling_info<sycl::info::event_profiling::command_start>();
+    auto end =
+        event.get_profiling_info<sycl::info::event_profiling::command_end>();
+    double duration = static_cast<double>(end - start) / NSEC_IN_MSEC;
+    std::cout << "Time = " << std::fixed << std::setprecision(3) << duration << " msec" << std::endl << std::endl;
+
     return false;
 }
 
@@ -130,11 +136,28 @@ void initMatrix(float *MA, float *MB, float *MC, int matSize)
     }
 }
 
+class int_gpu_device_selector : public device_selector
+{
+public:
+    int operator()(const sycl::device& dev) const override
+    {
+        if (dev.is_gpu()) {
+            auto vendorName = dev.get_info<sycl::info::device::vendor>();
+            if (vendorName.find("Intel") != std::string::npos) {
+                return 1;
+            }
+        }
+        return -1;
+    }
+};
+
 int main(int argc, char *argv[])
 {
     float *MA;
     float *MB;
     float *MC;
+
+    sycl::property_list prop_list{ sycl::property::queue::enable_profiling() };
     
     for (int i = 1024; i <= 8192; i += 1024) {
         MA = new float[i * i];
@@ -144,12 +167,12 @@ int main(int argc, char *argv[])
         std::cout << "Matrix Size N = " << i << std::endl;
 
         // Sequencial
-        /*initMatrix(MA, MB, MC, i);
+        initMatrix(MA, MB, MC, i);
         std::cout << "Sequential run" << std::endl;
         auto start = std::chrono::high_resolution_clock::now();
         block_host(MA, MB, MC, i, 32);
         auto end = std::chrono::high_resolution_clock::now();
-        std::cout << "Time = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl
+        std::cout << "Time = " << std::fixed << std::setprecision(3) << (double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0 << "ms" << std::endl
             << std::endl;
 
         // OPENMP
@@ -158,22 +181,20 @@ int main(int argc, char *argv[])
         start = std::chrono::high_resolution_clock::now();
         block_host_omp(MA, MB, MC, i, 32);
         end = std::chrono::high_resolution_clock::now();
-        std::cout << "Time = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl
-            << std::endl;*/
+        std::cout << "Time = " << std::fixed << std::setprecision(3) << (double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0 << "ms" << std::endl
+            << std::endl;
 
         // SYCL
-        auto platforms = sycl::platform::get_platforms();
 
-        std::vector<device> ds = { platforms[2].get_devices()[0]/*,  platforms[3].get_devices()[0]*/};
+        queue q1(cl::sycl::cpu_selector{}, prop_list);
+        initMatrix(MA, MB, MC, i);
+        std::cout << "OPENCL with " << q1.get_device().get_info<sycl::info::device::name>() << std::endl;
+        local_mxm(q1, MA, MB, MC, i, 32);
 
-        for (auto d : ds)
-        {
-            queue q(d);
-
-            initMatrix(MA, MB, MC, i);
-            std::cout << "OPENCL with " << q.get_device().get_info<sycl::info::device::name>() << std::endl;
-            local_mxm(q, MA, MB, MC, i, 32);
-        }
+        queue q2(int_gpu_device_selector{}, prop_list);
+        initMatrix(MA, MB, MC, i);
+        std::cout << "OPENCL with " << q2.get_device().get_info<sycl::info::device::name>() << std::endl;
+        local_mxm(q2, MA, MB, MC, i, 32);
 
         std::cout << std::endl;
 
